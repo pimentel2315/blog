6ee3cbd44fb4361c3fd07e188e0ca5e1e4c3ba7b
alondra.olvsan <alondra.olvsan@gmail.com>
2022-09-06 12:17:08 -0500

    modifaco view

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -23,18 +23,21 @@ def detallePost(request,slug):
 	post = get_object_or_404(Post,slug=slug)
 	return render(request,'post.html',{'detalle_post':post})
 
+
+	
+
 def generales(request):
 	queryset = request.GET.get("buscar")
 	posts = Post.objects.filter(
 		estado = True,
-		categoria = Categoria.objects.get(nombre__iexact = 'Generales')
+		
 	)
 	if queryset:
 		posts = Post.objects.filter(
 			Q(titulo__icontains = queryset) |
 			Q(decripcion__icontains = queryset),
 			estado = True,
-			categoria = Categoria.objects.get(nombre__iexact = 'Generales'),
+			categoria = Categoria.objects.get(nombre__iexact = 'Generales')
 			).distinct()
 	paginator = Paginator(posts,4)
 	page = request.GET.get('page')
@@ -45,7 +48,7 @@ def elementos_pasivos(request):
 	queryset = request.GET.get("buscar")
 	posts = Post.objects.filter(
 		estado = True,
-		categoria = Categoria.objects.get(nombre__iexact = 'componentes pasivos')
+		
 	)
 	if queryset:
 		posts = Post.objects.filter(
@@ -63,7 +66,7 @@ def elementos_activos(request):
 	queryset = request.GET.get("buscar")
 	posts = Post.objects.filter(
 		estado = True,
-		categoria = Categoria.objects.get(nombre__iexact = 'componentes activos')
+		
 	)
 	if queryset:
 		posts = Post.objects.filter(
